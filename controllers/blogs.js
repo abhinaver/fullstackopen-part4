12f192fe02cbaca17b1692306e5ac3e53098075c
abhinaver <abhinaver2004@gmail.com>
2025-06-28 21:04:26 +0530

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,7 +4,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-// Helper to extract token from Authorization header
 const getTokenFrom = (request) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
@@ -22,14 +21,14 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const token = getTokenFrom(request)
 
   let decodedToken
   try {
     decodedToken = jwt.verify(token, process.env.SECRET)
-  } catch {
+  } catch (err) {
     return response.status(401).json({ error: 'invalid token' })
   }
 
@@ -40,21 +39,22 @@ blogsRouter.post('/', async (request, response, next) => {
   const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
-    ...body,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
     user: user._id
   })
 
-  try {
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
+  const savedBlog = await blog.save()
 
-    const populated = await savedBlog.populate('user', ['username', 'name'])
-    response.status(201).json(populated)
-  } catch (error) {
-    next(error) // forward validation errors to errorHandler
-  }
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  const populatedBlog = await savedBlog.populate('user', { username: 1, name: 1 })
+  response.status(201).json(populatedBlog)
 })
+
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
